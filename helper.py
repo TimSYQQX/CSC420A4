93c9e6440b33ff60aaaeae9a55c1e819a5194eae
xiongti1 <tianyuxiong1997@gmail.com>
2019-11-28 21:31:09 -0500

    update

--- helper.py
+++ helper.py
@@ -1,22 +1,6 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from scipy import ndimage
-from matplotlib import cm
-import pandas as pd
-import warnings
-from numba import jit
-
-########################################################
-#
-#
-#     This file contains all the helper functions
-#
-#
-########################################################
-
-SIFT = cv2.xfeatures2d.SIFT_create()
-
 def apply_thres(matched_points):
     results = []
     for thres in np.linspace(1, 0, 500):
@@ -76,8 +60,50 @@ def draw_matching_lines(img1, img2, match, color=(255,0,0), size=5):
     h = img1.shape[1]
     img = np.hstack((img1, img2))
     for i in range(match.shape[0]):
-        pt1 = match[i, 0:2].astype(int)
-        pt2 = match[i, 2:4].astype(int)
+        pt1 = np.array(match[i, 0].pt).astype(int)
+        pt2 = np.array(match[i, 1].pt).astype(int)
         pt2[0] += h
         img = cv2.line(img, tuple(pt1), tuple(pt2), color, size)
     return img
+
+
+def get_sparse(mp, num=8, min_dist=400, best=True):
+    if best:
+        mp = sort_mp(mp)
+    pairs = []
+    for p in mp:
+        if len(pairs) == num:
+            return np.array(pairs)
+        if not pairs:
+            pairs.append(p)
+        else:
+            far = True
+            for pair in pairs:
+                if np.sqrt(np.sum((np.array(pair[0].pt)-np.array(p[0].pt))**2)) < min_dist:
+                    far = False
+                    break
+                if np.sqrt(np.sum((np.array(pair[1].pt)-np.array(p[1].pt))**2)) < min_dist:
+                    far = False
+                    break
+            if far:
+                pairs.append(p)  
+    print("No solution")
+
+def get_match(img1, img2):
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+    mp = match(des1, des2, kp1, kp2, L=2)
+    return mp
+
+def draw_match(img1, img2, mp, file):
+    pt1, pt2 = drawKeypoints(img1, mp[...,0], radius=50), drawKeypoints(img2, mp[...,1], radius=50)
+    line = draw_matching_lines(pt1, pt2, mp)
+    fig = figure(dpi=400)
+    imshow(line)
+    savefig(file)
+    
+def get_F(sparse):
+    pts1 = [i[0].pt for i in sparse]
+    pts2 = [i[1].pt for i in sparse]
+    F, _ = cv2.findFundamentalMat(pts1,pts2,cv2.FM_8POINT)
+    return F*2
